6f8da23a5f159e6d88e1a7e82f8d095dbaa72228
Vikas Choudhary <vikas.choudhary@nagarro.com>
2019-06-17 17:48:41 +0530

    Form Using Form Control

--- src/app/customers/customer.component.ts
+++ src/app/customers/customer.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { FormGroup, FormControl } from '@angular/forms';
 
 import { Customer } from './customer';
 
@@ -9,15 +9,31 @@ import { Customer } from './customer';
   styleUrls: ['./customer.component.css']
 })
 export class CustomerComponent implements OnInit {
+  customerForm: FormGroup;
   customer = new Customer();
 
   constructor() { }
 
   ngOnInit() {
+    this.customerForm = new FormGroup({
+      firstName: new FormControl(),
+      lastName: new FormControl(),
+      email: new FormControl(),
+      sendCatalog: new FormControl(true)
+    });
   }
 
-  save(customerForm: NgForm) {
-    console.log(customerForm.form);
-    console.log('Saved: ' + JSON.stringify(customerForm.value));
+  save() {
+    // console.log(customerForm.form);
+    // console.log('Saved: ' + JSON.stringify(customerForm.value));
+  }
+
+  populateTestData(): void {
+    this.customerForm.patchValue({
+      firstName: 'Vikas',
+      lastName: 'Choudhary',
+      email: 'test@email.com',
+      sendCatalog: false
+    });
   }
 }
