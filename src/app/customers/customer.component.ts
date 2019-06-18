e7cbe9070d0c84187b3b266e194d526dccc86b0a
Vikas Choudhary <vikas.choudhary@nagarro.com>
2019-06-18 12:03:04 +0530

    Validation on run time

--- src/app/customers/customer.component.ts
+++ src/app/customers/customer.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 import { Customer } from './customer';
 
@@ -16,9 +16,11 @@ export class CustomerComponent implements OnInit {
 
   ngOnInit() {
     this.customerForm = this.fb.group({
-      firstName: '',
-      lastName: '',
-      email: '',
+      firstName: ['', [Validators.required, Validators.minLength(3)]],
+      lastName: ['', [Validators.required, Validators.maxLength(50)]],
+      email: ['', [Validators.required, Validators.email]],
+      phone: [''],
+      notification: 'email',
       sendCatalog: true
     });
     // this.customerForm = new FormGroup({
@@ -41,5 +43,17 @@ export class CustomerComponent implements OnInit {
       email: 'test@email.com',
       sendCatalog: false
     });
+  }
+
+  setNotification(notifyVia: string): void {
+    const phoneControl = this.customerForm.get('phone');
+
+    if (notifyVia === 'text') {
+      phoneControl.setValidators(Validators.required);
+    } else {
+      phoneControl.clearValidators();
+    }
+
+    phoneControl.updateValueAndValidity();
   }
 }
